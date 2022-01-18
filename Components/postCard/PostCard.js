145be09524096c63cfc3ae39645155f002cc9663
Mike <mike88.merola@gmail.com>
2022-01-18 21:38:36 +0800

    added gradient to cards

--- Components/postCard/PostCard.js
+++ Components/postCard/PostCard.js
@@ -20,40 +20,42 @@ const PostCard = ({ title, content, date, id }) => {
   };
 
   return (
-    <div className="bg-white w-full dark:bg-brand-dark-grey-800 dark:border-brand-grey-800 border-b py-5 px-4 md:px-5 rounded-lg mb-3">
-      <UserHOC />
+    <div className=" rounded-lg bg-gradient-to-r p-1 from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] mb-3">
+      <div className="bg-white w-full  py-5 px-4 md:px-5 rounded-lg   ">
+        <UserHOC />
 
-      <div className="flex flex-row flex-wrap flex-grow-0 ">
-        <div className="flex-auto w-full pr-0 xl:w-auto xl:flex-1 xl:pr-5">
-          <Link href={`/Post/${id}`}>
-            <a className="mb-1 text-3xl font-semibold leading-tight tracking-tight text-brand-black dark:text-brand-grey-100">
-              {title}
-            </a>
-          </Link>
-          <p className="mb-2 text-xs font-medium text-brand-grey-600 dark:text-brand-grey-400">
-            {date}
-          </p>
-          <p className="max-w-full min-w-full mb-2 text-lg leading-snug tracking-tight break-words text-brand-grey-700 dark:text-brand-grey-400">
-            {content.slice(0, 250)}
-          </p>
-        </div>
-      </div>
-      <SignedIn>
-        <div className="flex flex-row items-center justify-between pt-4">
-          <div className="flex flex-row items-center justify-start space-x-2 w-full">
-            <Link href={`/editForm/${id}`}>
-              <a>Edit</a>
+        <div className="flex flex-row flex-wrap flex-grow-0">
+          <div className="flex-auto w-full pr-0 xl:w-auto xl:flex-1 xl:pr-5">
+            <Link href={`/Post/${id}`}>
+              <a className="mb-1 text-3xl font-semibold leading-tight tracking-tight text-brand-black dark:text-brand-grey-100">
+                {title}
+              </a>
             </Link>
-            <button
-              onClick={() => {
-                deleteBtn(id);
-              }}
-            >
-              Delete
-            </button>
+            <p className="mb-2 text-xs font-medium text-brand-grey-600 dark:text-brand-grey-400">
+              {date}
+            </p>
+            <p className="max-w-full min-w-full mb-2 text-lg leading-snug tracking-tight break-words text-brand-grey-700 dark:text-brand-grey-400">
+              {content.slice(0, 250)}
+            </p>
           </div>
         </div>
-      </SignedIn>
+        <SignedIn>
+          <div className="flex flex-row items-center justify-between pt-4">
+            <div className="flex flex-row items-center justify-start space-x-2 w-full">
+              <Link href={`/editForm/${id}`}>
+                <a>Edit</a>
+              </Link>
+              <button
+                onClick={() => {
+                  deleteBtn(id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </SignedIn>
+      </div>
     </div>
   );
 };
